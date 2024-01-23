16348cf1513e6880d692f1caacc263a1a5cc2a42
Mborsellino <104910179+Mborsellino@users.noreply.github.com>
2024-01-22 18:56:06 -0800

    Fixing test state (#3)
    * going to inspect test
    
    * Fixing DRS format and adding staging db tables
    
    * Fixing linting
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update test_terra.py
    
    * Update test_terra.py
    
    * Update test_terra.py
    
    * Update test_terra.py
    
    * Update test_terra.py
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Working against staging
    
    * Fixing most testing factors for staging
    
    * Fixing linting errors
    
    * Update main.yml
    
    * Fixing load-in method for service account
    
    * Update main.yml
    
    * Update test_terra.py
    
    * Creating tables
    
    * Linting
    
    * Re-enabling credentials
    
    * Fixing reportout
    
    * Fixing linting
    
    * Fixing no failures situation
    
    * Fixing sys exit
    
    * Updating version check

--- test/terra/test_terra.py
+++ test/terra/test_terra.py
@@ -7,9 +7,11 @@ import os
 import json
 import warnings
 import base64
+import sys
 
 # from test.bq import log_duration, Client
 from .utilities import Utilities
+from ..bq import log_duration, Client
 from terra_notebook_utils import drs
 
 
@@ -23,8 +25,8 @@ if STAGE == 'prod':
     BILLING_PROJECT = 'broad-integration-testing'
 elif STAGE == 'staging':
     GEN3_DOMAIN = 'https://staging.gen3.biodatacatalyst.nhlbi.nih.gov'
-    RAWLS_DOMAIN = 'https://rawls.dsde-alpha.broadinstitute.org'
-    ORC_DOMAIN = 'https://firecloud-orchestration.dsde-alpha.broadinstitute.org'
+    RAWLS_DOMAIN = 'https://rawls.dsde-staging.broadinstitute.org'
+    ORC_DOMAIN = 'https://firecloud-orchestration.dsde-staging.broadinstitute.org'
     BILLING_PROJECT = 'drs-billing-project'
 else:
     raise ValueError('Please set BDCAT_STAGE to "prod" or "staging".')
@@ -44,11 +46,13 @@ class TestTerra(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        '''COMMENT FOR LOCAL TESTING'''
         gcloud_cred_dir = os.path.expanduser('~/.config/gcloud')
         if not os.path.exists(gcloud_cred_dir):
             os.makedirs(gcloud_cred_dir, exist_ok=True)
         with open(os.path.expanduser('~/.config/gcloud/application_default_credentials.json'), 'w') as f:
-            f.write(base64.decodebytes(os.environ['TEST_MULE_CREDS'].encode('utf-8')).decode('utf-8'))
+            f.write(os.environ['TEST_MULE_CREDS'])
+        '''END COMMENT FOR LOCAL TESTING'''
         print(f'Terra [{STAGE}] Health Status:\n\n{json.dumps(Utilities.check_terra_health(ORC_DOMAIN), indent=4)}')
 
     @classmethod
@@ -58,7 +62,6 @@ class TestTerra(unittest.TestCase):
         except:  # noqa
             pass
 
-    # @retry(errors={requests.exceptions.HTTPError}, error_codes={409})
     def test_dockstore_import_in_terra(self):
         # import the workflow into terra
         response = Utilities.import_dockstore_wf_into_terra(RAWLS_DOMAIN, BILLING_PROJECT)
@@ -90,7 +93,6 @@ class TestTerra(unittest.TestCase):
         with self.subTest('Dockstore Check Workflow Not Seen'):
             self.assertFalse(wf_seen_in_terra)
 
-    @unittest.skip('This test needs to be updated.')
     def test_drs_workflow_in_terra(self):
         """This test runs md5sum in a fixed workspace using a drs url from gen3."""
         response = Utilities.run_workflow(RAWLS_DOMAIN, BILLING_PROJECT, STAGE)
@@ -106,14 +108,13 @@ class TestTerra(unittest.TestCase):
         # also configurable manually via MD5SUM_TEST_TIMEOUT if held in a pending state
         start = time.time()
         deadline = start + int(os.environ.get('MD5SUM_TEST_TIMEOUT', 60 * 60))
-        ''' UNCOMMENT THIS TOO'''
-        # table = f'platform-dev-178517.bdc.terra_md5_latency_min_{STAGE}'
+
+        table = f'unc-renci-bdc-itwg.bdc.terra_md5_latency_min_{STAGE}'
         while True:
             response = Utilities.check_workflow_status(rawls_domain=RAWLS_DOMAIN, billing_project=BILLING_PROJECT, submission_id=submission_id)
             status = response['status']
             if response['workflows'][0]['status'] == "Failed":
-                '''WHEN THIS GOES LIVE UNCOMMENT'''
-                # log_duration(table, time.time() - start)
+                log_duration(table, time.time() - start, True)
                 raise RuntimeError(f'The md5sum workflow did not succeed:\n{json.dumps(response, indent=4)}')
             elif status == 'Done':
                 break
@@ -124,14 +125,11 @@ class TestTerra(unittest.TestCase):
                           f"Checking again in 20 seconds.")
                     time.sleep(20)
                 else:
-                    print(json.dumps(response, indent=4))
-                    '''WHEN THIS GOES LIVE UNCOMMENT'''
-                    # log_duration(table, time.time() - start)
+                    log_duration(table, time.time() - start)
                     raise RuntimeError('The md5sum workflow run timed out.  '
                                        f'Expected 4 minutes, but took longer than '
                                        f'{float(now - start) / 60.0} minutes.')
-        '''WHEN THIS GOES LIVE UNCOMMENT'''
-        # log_duration(table, time.time() - start)
+        log_duration(table, time.time() - start)
         with self.subTest('Dockstore Workflow Run Completed Successfully'):
             if response['workflows'][0]['status'] != "Succeeded":
                 raise RuntimeError(f'The md5sum workflow did not succeed:\n{json.dumps(response, indent=4)}')
@@ -144,7 +142,7 @@ class TestTerra(unittest.TestCase):
         with self.subTest('Create a terra workspace.'):
             response = Utilities.create_terra_workspace(rawls_domain=RAWLS_DOMAIN, billing_project=BILLING_PROJECT, workspace=workspace_name)
             self.assertTrue('workspaceId' in response)
-            self.assertTrue(response['createdBy'] == 'biodata.integration.test.mule@gmail.com')
+            self.assertTrue(response['createdBy'] == 'biodata.itwg.test.mule@gmail.com')
 
         with self.subTest('Import static pfb into the terra workspace.'):
             response = Utilities.import_pfb(workspace=workspace_name,
@@ -174,19 +172,52 @@ class TestTerra(unittest.TestCase):
             response = Utilities.delete_terra_workspace(workspace=workspace_name, rawls_domain=RAWLS_DOMAIN, billing_project=BILLING_PROJECT)
             self.assertTrue(response.status_code == 404)
 
-    @unittest.skip('There seems to be an environment issue with this test in staging.')
     def test_public_data_access(self):
-        # this DRS URI only exists on staging/alpha and requires os.environ['TERRA_DEPLOYMENT_ENV'] = 'alpha'
-        drs.head('drs://dg.712C/fa640b0e-9779-452f-99a6-16d833d15bd0',
+        # this DRS URI only exists on staging and requires os.environ['TERRA_DEPLOYMENT_ENV'] = 'staging'
+        drs.head('drs://dg.712C:fa640b0e-9779-452f-99a6-16d833d15bd0',
                  workspace_name='DRS-Test-Workspace', workspace_namespace=BILLING_PROJECT)
 
-    @unittest.skip('This test needs to be updated.')
+    # @unittest.skip('Working')
     def test_controlled_data_access(self):
-        # this DRS URI only exists on staging/alpha and requires os.environ['TERRA_DEPLOYMENT_ENV'] = 'alpha'
-        drs.head('drs://dg.712C/04fbb96d-68c9-4922-801e-9b1350be3b94',
+        # this DRS URI only exists on staging/alpha and requires os.environ['TERRA_DEPLOYMENT_ENV'] = 'staging'
+        drs.head('drs://dg.712C:04fbb96d-68c9-4922-801e-9b1350be3b94',
                  workspace_name='DRS-Test-Workspace', workspace_namespace=BILLING_PROJECT)
 
 
 if __name__ == '__main__':
-    results = unittest.main(exit=False)
-    Utilities.report_out(results.result, WEBHOOK)
+    # This is manual and it is terrible. Look into changing eventually
+    test_list = {'test_dockstore_import_in_terra': '',
+                 'test_pfb_handoff_from_gen3_to_terra': '',
+                 'test_drs_workflow_in_terra': ''
+                 }
+    if STAGE == 'staging':
+        test_list['test_public_data_access'] = ''
+        test_list['test_controlled_data_access'] = ''
+
+    results = unittest.main(verbosity=2, exit=False)
+    # if len(results.result.failures) > 0 or len(results.result.errors) > 0:
+    #     Utilities.report_out(results.result, WEBHOOK)
+    timestamp = datetime.datetime.now()
+    client = Client()
+    all_failures = results.result.errors.extend(results.result.failures)
+    if all_failures is not None:
+        for test, status in all_failures:
+            # Unfortunately this is the only way to get the test method name from the TestCase
+            test_name = test._testMethodName
+            del test_list[test_name]
+            try:
+                # To create tables, skip all tests and set create to True:
+                if STAGE == 'staging':
+                    test_name = f'staging_{test_name}'
+                client.log_test_results(test_name, "failure", timestamp, create=True)
+            except Exception as e:
+                logger.exception('Failed to log test %r', test, exc_info=e)
+    for test_name in test_list.keys():
+        try:
+            # To create tables, skip all tests and set create to True:
+            if STAGE == 'staging':
+                test_name = f'staging_{test_name}'
+            client.log_test_results(test_name, "success", timestamp, create=True)
+        except Exception as e:
+            logger.exception('Failed to log test %r', test, exc_info=e)
+    sys.exit(not results.result.wasSuccessful())
